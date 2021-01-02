1a97d2a7dd1693991c60dcb9b440e9a74d8ee8c3
unknown <lwolczynski7@gmail.com>
2021-01-02 12:58:33 -0600

    Add results formatting

--- client/src/components/ComparisonTable.js
+++ client/src/components/ComparisonTable.js
@@ -1,10 +1,17 @@
 /* eslint-disable no-nested-ternary */
 /* eslint-disable react/prop-types */
 import React from "react";
-import { Container, Grid, Segment, Placeholder } from "semantic-ui-react";
+import {
+  Container,
+  Grid,
+  Segment,
+  Placeholder,
+  Header,
+  Image,
+} from "semantic-ui-react";
 
 const printPlaceholder = (child) => (
-  <Placeholder>
+  <Placeholder className="no-animation">
     <Placeholder.Header>{child}</Placeholder.Header>
   </Placeholder>
 );
@@ -20,45 +27,106 @@ const paragraphPlaceholder = printPlaceholder(
 
 const imagePlaceholder = printPlaceholder(<Placeholder.Image />);
 
-const linePlaceholder = printPlaceholder(<Placeholder.Line />);
+const linePlaceholder = (len = "medium") =>
+  printPlaceholder(<Placeholder.Line length={len} />);
 
 const rows = [
-  { name: "Title", jsonField: "Title", placeholder: linePlaceholder },
+  { name: "Title", jsonField: "Title", placeholder: linePlaceholder("long") },
   { name: "Poster", jsonField: "Poster", placeholder: imagePlaceholder },
   { name: "Plot", jsonField: "Plot", placeholder: paragraphPlaceholder },
-  { name: "Genre", jsonField: "Genre", placeholder: linePlaceholder },
-  { name: "Runtime", jsonField: "Runtime", placeholder: linePlaceholder },
-  { name: "Year", jsonField: "Year", placeholder: linePlaceholder },
-  { name: "Actors", jsonField: "Actors", placeholder: linePlaceholder },
-  { name: "Director", jsonField: "Director", placeholder: linePlaceholder },
-  { name: "IMDB score", jsonField: "IMDBScore", placeholder: linePlaceholder },
+  {
+    name: "Genre",
+    jsonField: "Genre",
+    placeholder: linePlaceholder("very long"),
+  },
+  {
+    name: "Runtime",
+    jsonField: "Runtime",
+    placeholder: linePlaceholder("short"),
+  },
+  { name: "Year", jsonField: "Year", placeholder: linePlaceholder("short") },
+  {
+    name: "Actors",
+    jsonField: "Actors",
+    placeholder: linePlaceholder("very long"),
+  },
+  {
+    name: "Director",
+    jsonField: "Director",
+    placeholder: linePlaceholder("long"),
+  },
+  {
+    name: "IMDB score",
+    jsonField: "IMDBScore",
+    placeholder: linePlaceholder("short"),
+  },
   {
     name: "Rotten Tomatoes score",
     jsonField: "RTScore",
-    placeholder: linePlaceholder,
+    placeholder: linePlaceholder("short"),
   },
-  { name: "Metascore", jsonField: "Metascore", placeholder: linePlaceholder },
-  { name: "Box Office", jsonField: "BoxOffice", placeholder: linePlaceholder },
-  { name: "Rated", jsonField: "Rated", placeholder: linePlaceholder },
-  { name: "Country", jsonField: "Country", placeholder: linePlaceholder },
-  { name: "Language", jsonField: "Language", placeholder: linePlaceholder },
-  { name: "Awards", jsonField: "Awards", placeholder: linePlaceholder },
-  { name: "Writer", jsonField: "Writer", placeholder: linePlaceholder },
-  { name: "Released", jsonField: "Released", placeholder: linePlaceholder },
-  { name: "Type", jsonField: "Type", placeholder: linePlaceholder },
+  { name: "Metascore", jsonField: "Metascore", placeholder: linePlaceholder() },
+  {
+    name: "Box Office",
+    jsonField: "BoxOffice",
+    placeholder: linePlaceholder("short"),
+  },
+  {
+    name: "Rated",
+    jsonField: "Rated",
+    placeholder: linePlaceholder("very short"),
+  },
+  {
+    name: "Country",
+    jsonField: "Country",
+    placeholder: linePlaceholder("medium"),
+  },
+  {
+    name: "Language",
+    jsonField: "Language",
+    placeholder: linePlaceholder("medium"),
+  },
+  { name: "Awards", jsonField: "Awards", placeholder: linePlaceholder("long") },
+  { name: "Writer", jsonField: "Writer", placeholder: linePlaceholder("long") },
+  {
+    name: "Released",
+    jsonField: "Released",
+    placeholder: linePlaceholder("medium"),
+  },
+  { name: "Type", jsonField: "Type", placeholder: linePlaceholder("short") },
   {
     name: "Total seasons",
     jsonField: "totalSeasons",
-    placeholder: linePlaceholder,
+    placeholder: linePlaceholder("very short"),
   },
-  { name: "See on IMDB", jsonField: "IMDBLink", placeholder: linePlaceholder },
+  {
+    name: "See on IMDB",
+    jsonField: "IMDBLink",
+    placeholder: linePlaceholder("medium"),
+  },
 ];
+
+const formatData = (type, data) => {
+  switch (type) {
+    case "Title":
+      return <strong>{data}</strong>;
+    case "Poster":
+      if (data === "N/A") {
+        return <div className="no-image center">No image</div>;
+      }
+      return <Image src={data} />;
+    default:
+      return <span>{data}</span>;
+  }
+};
 
 // eslint-disable-next-line react/prop-types
 const ComparisionTable = ({ movies }) => {
   const printFirstColumn = (row) => (
     <Grid.Column>
-      <div className="full center">{row.name}</div>
+      <div className="full center">
+        <Header as="h3">{row.name}</Header>
+      </div>
     </Grid.Column>
   );
 
@@ -66,10 +134,10 @@ const ComparisionTable = ({ movies }) => {
     // eslint-disable-next-line react/prop-types
     movies.map((movie, index) => (
       <Grid.Column key={index}>
-        <Segment raised className="full">
+        <Segment raised textAlign="center" className="full">
           {movie.Title
             ? movie[row.jsonField]
-              ? movie[row.jsonField]
+              ? formatData(row.jsonField, movie[row.jsonField])
               : "N/A"
             : row.placeholder}
         </Segment>
