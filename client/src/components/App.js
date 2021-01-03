24e56ccdf5f980b33d8aaefa104deed98f5d57e6
unknown <lwolczynski7@gmail.com>
2021-01-03 10:38:50 -0600

    Make navbar sticky

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { createRef } from "react";
+import { Sticky } from "semantic-ui-react";
 import Navbar from "./Navbar";
 import Top from "./Top";
 import Comparer from "./Comparer";
 import Footer from "./Footer";
 
-const App = () => (
-  <>
-    <Top />
-    <Navbar />
-    <Comparer />
-    <Footer />
-  </>
-);
+const App = () => {
+  const contextRef = createRef();
+
+  return (
+    <div ref={contextRef}>
+      <Top />
+      <Sticky context={contextRef}>
+        <Navbar />
+      </Sticky>
+      <Comparer />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
