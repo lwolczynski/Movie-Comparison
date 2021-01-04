2bd616831b66fe876bb43bb58ee3ba7a891cd2ea
unknown <lwolczynski7@gmail.com>
2021-01-04 15:24:58 -0600

    Add footer content

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { Container, Segment } from "semantic-ui-react";
+import { Container, Segment, List } from "semantic-ui-react";
 
 const Footer = () => (
-  <Segment inverted attached>
+  <Segment inverted attached textAlign="center">
     <Container>
-      <p>Footer</p>
+      <div>© 2021 created by lwolczynski</div>
+      <List link inverted divided horizontal size="small">
+        <List.Item href="https://github.com/lwolczynski">GitHub</List.Item>
+        <List.Item href="#">Portfolio</List.Item>
+      </List>
     </Container>
   </Segment>
 );
