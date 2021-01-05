152d5c27ac376874b7af78f845558fabd88e05b3
unknown <lwolczynski7@gmail.com>
2021-01-04 18:48:28 -0600

    Add the logo font and a css class to style the app name

--- client/src/components/Top.js
+++ client/src/components/Top.js
@@ -4,7 +4,9 @@ import { Header, Container, Divider } from "semantic-ui-react";
 const Top = () => (
   <Container>
     <Divider hidden />
-    <Header as="h1">Pick-a-mov</Header>
+    <Header as="h1" className="brand">
+      Pick-a-mov
+    </Header>
     <p>
       Cant decide between films? Compare them and{" "}
       <span style={{ textDecorationLine: "underline" }}>pick a mov</span>ie to
