92866f939bd2a023b4f1ff565fac0dbe3adcfff5
unknown <lwolczynski7@gmail.com>
2021-01-09 15:23:35 -0600

    Refactor Carousel component

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+/* eslint-disable react/prop-types */
+import React, { useContext, useRef } from "react";
 import { Image, Card } from "semantic-ui-react";
 import Slider from "react-slick";
 import carouselData from "./carouselData";
@@ -7,32 +8,43 @@ import PosterContext from "./PosterContext";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const generateCards = () => {
+// eslint-disable-next-line react/display-name
+const Poster = ({ item }) => {
   const { setPosterClicked } = useContext(PosterContext);
-
-  const shuffled = carouselData.sort(() => 0.5 - Math.random());
-  const selected = shuffled.slice(0, 15);
-  return selected.map((item, index) => (
-    <div className="carousel-div" key={index}>
-      <Card onClick={() => setPosterClicked({ ...item })}>
-        <Image className="carousel-image" src={item.poster} />
-      </Card>
-    </div>
-  ));
-};
-const settings = {
-  infinite: true,
-  centerMode: true,
-  autoplay: true,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  variableWidth: true,
-  autoplaySpeed: 3000,
-  nextArrow: <></>,
-  prevArrow: <></>,
-  cssEase: "linear",
+  return (
+    <Card onClick={() => setPosterClicked({ ...item })}>
+      <Image className="carousel-image" src={item.poster} />
+    </Card>
+  );
 };
 
-const Carousel = () => <Slider {...settings}>{generateCards()}</Slider>;
+// eslint-disable-next-line react/display-name
+const Carousel = React.memo(() => {
+  const selected = useRef(
+    carouselData.sort(() => 0.5 - Math.random()).slice(0, 15)
+  );
+
+  const settings = useRef({
+    infinite: true,
+    centerMode: true,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplaySpeed: 3000,
+    nextArrow: <></>,
+    prevArrow: <></>,
+    cssEase: "linear",
+  });
+
+  const generateCards = () =>
+    selected.current.map((item, index) => (
+      <div className="carousel-div" key={index}>
+        <Poster item={{ ...item }} />
+      </div>
+    ));
+
+  return <Slider {...settings.current}>{generateCards()}</Slider>;
+});
 
 export default Carousel;
