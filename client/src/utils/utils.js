9af79632bc4872747ff122219f815bc27ccec127
unknown <lwolczynski7@gmail.com>
2021-01-12 18:09:58 -0600

    Add to smoothscroll library to allow smooth scroll on all devices

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,6 +1,6 @@
-// import smoothscroll from "smoothscroll-polyfill";
+import smoothscroll from "smoothscroll-polyfill";
 
-// smoothscroll.polyfill();
+smoothscroll.polyfill();
 
 const scrollToElement = (element) => {
   const y = element.getBoundingClientRect().top + window.pageYOffset + 1;
