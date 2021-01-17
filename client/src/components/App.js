0bfc8643d7c9fd7f6ff07359af319a123715e866
unknown <lwolczynski7@gmail.com>
2021-01-16 18:15:27 -0600

    Add Google Analytics

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,14 @@
 import React, { useState, createRef } from "react";
+import ReactGA from "react-ga";
 import Navbar from "./Navbar";
 import Top from "./Top";
 import Comparer from "./Comparer";
 import Footer from "./Footer";
 import PosterContext from "./PosterContext";
 import scrollToElement from "../utils/utils";
+
+ReactGA.initialize(process.env.REACT_APP_GA_ID);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const App = () => {
   const mainRef = createRef();
