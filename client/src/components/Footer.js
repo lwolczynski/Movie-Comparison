7bfb1a05a77df112c37acab2f9502d2a61ba1ff5
unknown <lwolczynski7@gmail.com>
2021-01-18 11:57:10 -0600

    Change to links in footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,8 +6,12 @@ const Footer = () => (
     <Container>
       <div>© 2021 created by lwolczynski</div>
       <List link inverted divided horizontal size="small">
-        <List.Item href="https://github.com/lwolczynski">GitHub</List.Item>
-        <List.Item href="#">Portfolio</List.Item>
+        <List.Item href="https://github.com/lwolczynski" target="_blank">
+          GitHub
+        </List.Item>
+        <List.Item href="https://lwolczynski.com" target="_blank">
+          Portfolio
+        </List.Item>
       </List>
     </Container>
   </Segment>
